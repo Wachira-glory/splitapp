bfd83f83a326308126fe0feee9b0833d76199ca3
Wachira-glory <wachiraglorywairimu@gmail.com>
2025-10-09 11:23:13 +0300

    Added the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,8 @@ export default function HomePage() {
   const router = useRouter()
 
   useEffect(() => {
-    if (!isLoading) {
-      if (user) {
-        router.push("/dashboard")
-      } else {
-        router.push("/login")
-      }
-    }
-  }, [user, isLoading, router])
-
-  if (isLoading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-600"></div>
-      </div>
-    )
-  }
+    router.push("/landing")
+  }, [router])
 
   return null
 }
